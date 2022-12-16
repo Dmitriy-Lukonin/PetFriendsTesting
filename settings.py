0fb80c370406ba0c9d850d54d5d304199b92a22a
Dmitriy-Lukonin <lukonin.d.v@gmail.com>
2022-12-16 15:59:09 +0500

    Версия 1.3 Изменены названия фото и отредактирован список images

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ no_valid_email = "lukonin.d.v@gmail.com"
 no_valid_password = "POSTman1"
 
 images = ["150.jpg", "151.jpg", "152.jpeg", "153.jpeg", "154.jpeg", "155.jpg", "156.jpg", "157.jpg", "158.jpeg",
-          "cat1.jpg", "P1040103.jpeg"]
+          "160.jpg", "162.jpeg", "163.jpeg", "164.jpeg"]
 
 print(random.choice(images))
 
